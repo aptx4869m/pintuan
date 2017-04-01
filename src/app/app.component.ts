badbbb5f4e0e8473f45351e89485ce54d74ef37e
tinayuangao <yuan.gao.tina@gmail.com>
2017-04-01 02:16:54 +0000

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,137 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import * as wilddog from 'wilddog';
+
+class Item {
+  name: string = '';
+  img: string = '';
+  price: number = 0;
+  stock: number= 0;
+  total: number = 0;
+  
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class People {
+  name: string = '';
+  buy: Map<string, number> = new Map<string, number>();
+  
+  constructor(name: string) {
+    this.name = name;
+  }
+  total: number = 0;
+}
+
+class Collection {
+  name: string = '';
+  description: string = '';
+  img: string = '';
+  rate: number = 1;
+  items: Item[] = [];
+  peoples: People[] = [];
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  isOwner: boolean = true;
+  collection: Collection = new Collection();
+  currentPeople: People;
+  ref = wilddog.sync().ref();
+
+  constructor() {  }
+  
+  deleteItem(name: string) {
+    console.log(name);
+    let index = -1;
+    this.collection.items.forEach((item, i) => {
+      if (item.name === name) {
+        index = i;
+      }
+    });
+    if (index > -1) {
+      console.log(index);
+      this.collection.items.splice(index, 1);
+    }
+    this.collection.peoples.forEach((p) => {
+      delete p.buy[name]
+    });
+  }
+  
+  addItem(name: string) {
+    // check name
+    if (name && name.trim() !== '') {
+      name = name.trim();
+      let notexists = this.collection.items.filter((i) => i.name === name).length == 0;
+      console.log(notexists);
+      if (notexists) {
+        this.collection.items.push(new Item(name));
+      }
+    }
+  }
+  
+  addPeople(name: string) {
+    if (name && name.trim() !== '') {
+      let matches = this.collection.peoples.filter((i) => i.name === name);
+      if (matches.length == 0) {
+        let newPeople = new People(name);
+        this.collection.peoples.push(newPeople);
+        if (!this.isOwner) {
+          this.currentPeople = newPeople;
+        }
+      } else if (!this.isOwner) {
+        this.currentPeople = matches[0];
+      }
+    }
+  }
+  
+  deletePeople(name: string) {
+    console.log(name);
+    let index = -1;
+    this.collection.peoples.forEach((people, i) => {
+      if (people.name === name) {
+        index = i;
+      }
+    });
+    if (index > -1) {
+      console.log(index);
+      this.collection.peoples.splice(index, 1);
+    }
+    if (this.currentPeople.name === name) {
+      this.currentPeople = null;
+    }
+  }
+  
+  submit() {
+    this.calculate();
+    if (this.isOwner) {
+      console.log(this.collection);
+    } else if (this.currentPeople) {
+      let matches = this.collection.peoples.filter((p) => p.name === this.currentPeople.name);
+      console.log(matches);
+    }
+    
+  }
+  
+  calculate() {
+    this.collection.peoples.forEach((people) => {
+      people.total = 0;
+    }); 
+    this.collection.items.forEach((item) => {
+      item.total = 0;
+    });
+    this.collection.items.forEach((item) => {
+      this.collection.peoples.forEach((people) => {
+        let numberItems = people.buy[item.name];
+        if (numberItems) {
+          item.total += numberItems;
+          people.total += item.price * numberItems;
+        }
+      });
+    })
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import * as wilddog from 'wilddog';
 
+import {UserService} from './user.service';
+import {GoodsService} from './goods.service';
 import { AppComponent } from './app.component';
+
+var config = {
+  syncURL: "https://<appId>.wilddogio.com" //输入节点 URL
+};
+wilddog.initializeApp(config);
 
 @NgModule({
   declarations: [
@@ -12,9 +21,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [ UserService, GoodsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
